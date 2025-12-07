2e3d3390fad56445128bb0b02ff5cbbfff694b4a
OD-OD <od7730110@gmail.com>
2025-12-07 21:27:27 +0530

    upload CompanySetupSuccessServlet

---
+++ Hireza/src/main/java/controller/CompanySetupSuccessServlet.java
@@ -0,0 +1,30 @@
+package controller;
+
+import model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/company/profile/setup/success")
+public class CompanySetupSuccessServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+
+        if (user == null || !"Employer".equals(user.getRole())) {
+            response.sendRedirect(request.getContextPath() + "/jobseeker/signin.jsp");
+            return;
+        }
+
+        request.getRequestDispatcher("/Employer/setup_success.jsp").forward(request, response);
+    }
+}
